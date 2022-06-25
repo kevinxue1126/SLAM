b2c558330b054286269b86e8a11d2781d7cc8844
Kevin Xue <kevinxue1126@gmail.com>
2022-06-25 23:52:14 +0800

    Update pointcloudmapping.h

--- include/pointcloudmapping.h
+++ include/pointcloudmapping.h
@@ -1,5 +1,5 @@
 /* This file is part of ORB-SLAM2-SSD-Semantic.
- * 新添加的语义地图构建 
+ * Newly added semantic map building
  * 
  */
 
@@ -12,16 +12,16 @@
 #include <pcl/common/common.h>
 #include <pcl/point_types.h>
 
-#include <pcl/filters/voxel_grid.h> // 体素格滤波
-#include <pcl/filters/statistical_outlier_removal.h>// 统计滤波器 
-#include <pcl/filters/extract_indices.h>            // 根据点云索引提取对应的点云
-#include <pcl/filters/filter.h>                     // 取出nan点 变无序点
-#include <pcl/io/pcd_io.h> // 读写点云
-#include <pcl/visualization/pcl_visualizer.h> // PCLVisualizer 点云可视化器
+#include <pcl/filters/voxel_grid.h> // Voxel Lattice Filtering
+#include <pcl/filters/statistical_outlier_removal.h>// statistical filter
+#include <pcl/filters/extract_indices.h>            // Extract the corresponding point cloud according to the point cloud index
+#include <pcl/filters/filter.h>                     // Take out nan points and change them into disordered points
+#include <pcl/io/pcd_io.h> // Read and write point clouds
+#include <pcl/visualization/pcl_visualizer.h> // PCLVisualizer 
 
 #include <condition_variable>
 
-#include "Thirdparty/ncnn/ncnn_dect.h"// ncnn ssd目标检测
+#include "Thirdparty/ncnn/ncnn_dect.h"// ncnn ssd target detection
 
 #include <Eigen/Core>
 
@@ -29,30 +29,30 @@
 
 using namespace ORB_SLAM2;
 
-// 目标语义信息
+// target semantic information
 typedef struct Cluster
 {
- std::string object_name; // 物体类别名
- int class_id;            // 对应类别id
- float prob;              // 置信度
- Eigen::Vector3f minPt;   // 所有点中最小的x值，y值，z值
- Eigen::Vector3f maxPt;   // 所有点中最大的x值，y值，z值
- Eigen::Vector3f centroid;// 点云中心点 
+ std::string object_name; // object class name
+ int class_id;            // corresponding category id
+ float prob;              // Confidence
+ Eigen::Vector3f minPt;   // The smallest x value, y value, z value among all points
+ Eigen::Vector3f maxPt;   // The largest x value, y value, z value among all points
+ Eigen::Vector3f centroid;// point cloud center point
  bool operator ==(const std::string &x);
 } Cluster;
 
 class PointCloudMapping
 {
 public:
-    typedef pcl::PointXYZRGB PointT;// 点类型 xyzrgb 点+颜色
-    typedef pcl::PointCloud<PointT> PointCloud;// 点云类型
+    typedef pcl::PointXYZRGB PointT;// point type xyzrgb point + color
+    typedef pcl::PointCloud<PointT> PointCloud;// Point cloud type
 
-    PointCloudMapping( double resolution_ );// 类初始化(构造)函数
+    PointCloudMapping( double resolution_ );// Class initialization (constructor) function
   
-    // 插入一个keyframe，会更新一次地图
+    // Inserting a keyframe will update the map once
     void insertKeyFrame( KeyFrame* kf, cv::Mat& color, cv::Mat& depth, cv::Mat& imgRGB);
-    void shutdown();// 相当于类析构函数
-    void viewer();  // 可视化点云函数
+    void shutdown();// Equivalent to class destructor
+    void viewer();  // Visualize point cloud functions
     void update();
 
 protected:
@@ -62,50 +62,48 @@ protected:
                                     cv::Mat depth_img, 
                                     const cv::Rect_<float>& rect, const cv::Scalar& scalar,
                                     pcl::PointIndices & indices);
-    void sem_merge(Cluster cluster); // 融合点云团
-    void add_cube(void);// 可视化器中加入 3d标识框
+    void sem_merge(Cluster cluster); // fused point cloud
+    void add_cube(void);// Add a 3d logo frame to the visualizer
 
-    PointCloud::Ptr globalMap;        // 点云地图指针   boost::share_ptr
-    std::shared_ptr<thread>  viewerThread; // 点云可视化线程 std::shared_ptr
+    PointCloud::Ptr globalMap;        // Point cloud map pointer
+    std::shared_ptr<thread>  viewerThread; // Point cloud visualization thread
 
-    bool    shutDownFlag    =false;   // 关闭标志
-    mutex   shutDownMutex;            // 关闭 线程互斥锁
+    bool    shutDownFlag    =false;   // close sign
+    mutex   shutDownMutex;            // close thread mutex
  
     condition_variable  keyFrameUpdated; 
-// 关键帧更新 <condition_variable> 头文件主要包含了与条件变量相关的类和函数。
-// 全局条件变量. 用于多线程之间的 相互等待！！！！！！！
-    // condition_variable 类 参考 https://msdn.microsoft.com/zh-cn/magazine/hh874752(v=vs.120)
-    mutex               keyFrameUpdateMutex;// 关键帧更新  互斥锁
+    // Keyframe Updates The <condition_variable> header file mainly contains classes and functions related to condition variables.
+    mutex               keyFrameUpdateMutex;
 
     // data to generate point clouds
-    std::vector<KeyFrame*>       keyframes;  // 关键帧指针 数组
-    std::vector<cv::Mat>         colorImgs;  // 灰度图    数组
-    std::vector<cv::Mat>         depthImgs;  // 深度图    数组
-    std::vector<cv::Mat>         RGBImgs;    // 深度图    数组
-    mutex                   keyframeMutex;   // 关键帧 互斥锁
+    std::vector<KeyFrame*>       keyframes;  // Keyframe pointer
+    std::vector<cv::Mat>         colorImgs;  // Grayscale
+    std::vector<cv::Mat>         depthImgs;  // depth map    
+    std::vector<cv::Mat>         RGBImgs;    // depth map    
+    mutex                   keyframeMutex;   // Keyframe
     uint16_t                lastKeyframeSize =0;
 
-    double resolution = 0.04;      // 默认点云地图精度    用于设置体素格子的边长大小
-    pcl::VoxelGrid<PointT>  voxel; // 点对应的 体素格滤波对象
+    double resolution = 0.04;      // Default point cloud map accuracy
+    pcl::VoxelGrid<PointT>  voxel; // The voxel filter object corresponding to the point
 
-    //shared_ptr<std::vector<cv::Scalar>> colors_ptr;// 不同物体颜色对象   std::shared_ptr
-    std::vector<cv::Scalar> colors_;   // 每种物体的颜色
-    std::vector<float>      obj_size_; // 每种物体的大小
-    std::shared_ptr<Detector> ncnn_detector_ptr; // ncnn ssd目标检测std::shared_ptr
+    //shared_ptr<std::vector<cv::Scalar>> colors_ptr;// different objects color objects   std::shared_ptr
+    std::vector<cv::Scalar> colors_;   // the color of each object
+    std::vector<float>      obj_size_; // size of each object
+    std::shared_ptr<Detector> ncnn_detector_ptr; // ncnn ssd target detection std::shared_ptr
     
 
-    std::vector<Cluster> clusters;// 语义点云指针数组
-    pcl::ExtractIndices<PointT> extract_indices;// 索引提取点云器  设置为成员对象
-    // pcl::VoxelGrid<PointT>  voxel_; // 体素格滤波对象  上面已经设置了
-    pcl::StatisticalOutlierRemoval<PointT> stat; // 统计学滤波，剔除离群点
+    std::vector<Cluster> clusters;// Array of semantic point cloud pointers
+    pcl::ExtractIndices<PointT> extract_indices;// Index extraction point cloud
+    // pcl::VoxelGrid<PointT>  voxel_; // voxel filter object
+    pcl::StatisticalOutlierRemoval<PointT> stat; // Statistical filtering to remove outliers
 
     
     std::shared_ptr<pcl::visualization::PCLVisualizer> pcl_viewer_prt;
     std::shared_ptr<pcl::PCDWriter>                    pcd_writer_ptr;
 
 
-    //std::shared_ptr<thread>  showThread; // 点云更新线程 std::shared_ptr
-    int map_state_ok;// 地图是否准备好
+    // Is the map ready
+    int map_state_ok;
 };
 
 #endif // POINTCLOUDMAPPING_H
