74b3f922dac1d59bc9e73fa683ea7ed210a88c9f
Kevin Xue <kevinxue1126@gmail.com>
2022-06-25 23:30:49 +0800

    Update MapPoint.h

--- include/MapPoint.h
+++ include/MapPoint.h
@@ -1,11 +1,7 @@
 /**
 * This file is part of ORB-SLAM2.
-* 地图点  普通帧上的地图点
-*               关键帧上的地图点
-* 创建 地图点    观测帧集合   最优的描述子
-* 环点检测 被观测次数 小于2  删除地图点
-* 地图点距离 参考帧相机中心的 相对 坐标
-* 地图点 相对参考帧 相机中心  在 图像金字塔上 每层 各个尺度空间的距离
+* Map point on normal frame
+* 
 * 
 */
 
@@ -22,33 +18,30 @@
 namespace ORB_SLAM2
 {
 
-class KeyFrame;// 关键帧
-class Map;// 地图
-class Frame;//普通帧
+class KeyFrame;
+class Map;
+class Frame;
 
 
 class MapPoint
 {
 public:
+	
   
-  // 构造函数（地图点3D坐标及其参考帧）
-  
-	// 创建关键帧地图点   世界坐标点     所属关键帧    所属地图
-	// 参考帧是关键帧，该地图点将于许多帧关键帧对应，建立关键帧之间的共视关系
+    // Constructor (3D coordinates of map points and their reference frames)
     MapPoint(const cv::Mat &Pos, KeyFrame* pRefKF, Map* pMap);
     
-   // 参考帧是普通帧，该地图点只与当前普通帧的特征点对应 
-   // 创建普通帧地图点    世界坐标点       所属地图   所属普通 帧      帧id
+   // The reference frame is an ordinary frame, and the map point only corresponds to the feature point of the current ordinary frame
     MapPoint(const cv::Mat &Pos,  Map* pMap, Frame* pFrame, const int &idxF);
 
     
-    void SetWorldPos(const cv::Mat &Pos);//设置点
-    cv::Mat GetWorldPos();// 获取点
+    void SetWorldPos(const cv::Mat &Pos);
+    cv::Mat GetWorldPos();
 
-    cv::Mat GetNormal();// 点 相对于 相机光心坐标 
-    KeyFrame* GetReferenceKeyFrame();// 获取参考关键帧
+    cv::Mat GetNormal();
+    KeyFrame* GetReferenceKeyFrame();
 
-    std::map<KeyFrame*,size_t> GetObservations();// 观测帧
+    std::map<KeyFrame*,size_t> GetObservations();
     int Observations();
 
     void AddObservation(KeyFrame* pKF,size_t idx);
@@ -66,18 +59,15 @@ public:
     void IncreaseVisible(int n=1);
     void IncreaseFound(int n=1);
     
-    //GetFoundRatio低表示该地图点在很多关键帧的视野范围内，但是没有匹配上很多特征点。
+    //A low GetFoundRatio means that the map point is within the field of view of many keyframes, but not many feature points are matched.
     float GetFoundRatio();
     inline int GetFound(){
         return mnFound;
     }
 
     void ComputeDistinctiveDescriptors();
-// 1 在观测到该地图点的多个特征点中（对应多个关键帧），
-     // 挑选出区分度最高的描述子，作为这个地图点的描述子；
-    cv::Mat GetDescriptor();// 地图点对应的描述子
+    cv::Mat GetDescriptor();
     
-// 2. 计算地图点平均观测方向和深度 （在所有观测帧上）
     void UpdateNormalAndDepth();
 
     float GetMinDistanceInvariance();
@@ -90,12 +80,11 @@ public:
     static long unsigned int nNextId;
     long int mnFirstKFid;
     long int mnFirstFrame;
-    int nObs;//    观测到该地图点的相机数  关键帧数量
+    int nObs;
 
     // Variables used by the tracking
-    // 跟踪 到的 参数 
-    float mTrackProjX;//匹配点 x偏移
-    float mTrackProjY;//匹配点 y偏移
+    float mTrackProjX;
+    float mTrackProjY;
     float mTrackProjXR;
     bool mbTrackInView;
     int mnTrackScaleLevel;
@@ -105,10 +94,10 @@ public:
 
     // Variables used by local mapping
     long unsigned int mnBALocalForKF;
-    long unsigned int mnFuseCandidateForKF;// 地图点 和 哪一帧的 地图点有融合 的标记
+    long unsigned int mnFuseCandidateForKF;
 
     // Variables used by loop closing
-    long unsigned int mnLoopPointForKF;// 闭环检测标志
+    long unsigned int mnLoopPointForKF;
     long unsigned int mnCorrectedByKF;
     long unsigned int mnCorrectedReference;    
     cv::Mat mPosGBA;
@@ -123,25 +112,24 @@ protected:
      cv::Mat mWorldPos;
 
      // Keyframes observing the point and associated index in keyframe
-     std::map<KeyFrame*,size_t> mObservations;//  能够观测到次点的 关键帧  观测帧
+     std::map<KeyFrame*,size_t> mObservations;
 
      // Mean viewing direction
      cv::Mat mNormalVector;
 
-     // Best descriptor to fast matching 地图点 特征描述子
+     // Best descriptor to fast matching 
      cv::Mat mDescriptor;
 
      // Reference KeyFrame
-     KeyFrame* mpRefKF;// 参考关键帧
+     KeyFrame* mpRefKF;
 
      // Tracking counters
      int mnVisible;
      int mnFound;
 
      // Bad flag (we do not currently erase MapPoint from memory)
-     // 坏点标志  被观测到的次数  小于2
      bool mbBad;
-     MapPoint* mpReplaced;//坏点的 代替 点?
+     MapPoint* mpReplaced;
 
      // Scale invariance distances
      float mfMinDistance;
