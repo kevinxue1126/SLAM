d07ba28be5549516bca10d0e79970e413177d966
Kevin Xue <kevinxue1126@gmail.com>
2022-06-26 00:38:00 +0800

    Update draw_trajectory.py

--- tool/draw_trajectory.py
+++ tool/draw_trajectory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
-# 绘制 GT、SRC、FLOW 相机3D轨迹
-# 使用 python draw_trajectory.py
+# Draw GT, SRC, FLOW camera 3D trajectories
+# Use python draw_trajectory.py
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d
@@ -17,7 +17,7 @@ for line in f:
     x.append( float(data[1] ) )
     y.append( float(data[2] ) )
     z.append( float(data[3] ) )
-# 起始位置 -0.6885 -3.1192 1.4248
+# starting point -0.6885 -3.1192 1.4248
 
 
 f2 = open("./flow.txt")
@@ -57,14 +57,14 @@ for line in f4:
     z4.append( float(data[3] ) +1.4248)
     
 ax = plt.subplot( 111, projection='3d')
-ax.plot(x,y,z,'r',label='groundtruth')  # 红色线条
-ax.plot(x2,y2,z2,'g',label='flow')  # 绿色线条
-ax.plot(x3,y3,z3,'b',label='src')  # 蓝色线条
-ax.plot(x4,y4,z4,'y',label='geom')  # 黄色线条
+ax.plot(x,y,z,'r',label='groundtruth')  # Red lines
+ax.plot(x2,y2,z2,'g',label='flow')  # Green line
+ax.plot(x3,y3,z3,'b',label='src')  # Blue line
+ax.plot(x4,y4,z4,'y',label='geom')  # Yellow line
 
-ax.legend(loc='best')# 显示图例 label=' ' 
-ax.set_zlabel('Z')  # 坐标轴
+ax.legend(loc='best')# Show legend label=' ' 
+ax.set_zlabel('Z')  # Axis
 ax.set_ylabel('Y')
 ax.set_xlabel('X')
-ax.set_title('trajectory')#图标题  
+ax.set_title('trajectory')#Figure title 
 plt.show()
