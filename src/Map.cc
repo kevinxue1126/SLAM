9ccc933fa36f1fa996fa175b04ca0facc0ba0acd
Kevin Xue <kevinxue1126@gmail.com>
2022-06-26 09:51:58 +0800

    Update Map.cc

--- src/Map.cc
+++ src/Map.cc
@@ -1,7 +1,7 @@
 /**
 * This file is part of ORB-SLAM2.
-* 地图 管理 关键帧 地图点
-*  添加/删除  关键帧/地图点
+* map,manage,keyframe and map point
+* add/delete  keyframe/map point
 */
 
 #include "Map.h"
@@ -10,7 +10,7 @@
 
 namespace ORB_SLAM2
 {
-// 初始 最大关键帧id = 0  最大变化id = 0
+// initial, max keyframe id = 0, max change id = 0
 Map::Map():mnMaxKFid(0),mnBigChangeIdx(0)
 {
 }
@@ -18,26 +18,25 @@ Map::Map():mnMaxKFid(0),mnBigChangeIdx(0)
 void Map::AddKeyFrame(KeyFrame *pKF)
 {
     unique_lock<mutex> lock(mMutexMap);
-    mspKeyFrames.insert(pKF);// set 集合 插入关键帧
-    if(pKF->mnId > mnMaxKFid)// 关键帧 id
-        mnMaxKFid=pKF->mnId;// 最大关键帧 的id
+    mspKeyFrames.insert(pKF);// set collection, insert keyframes
+    if(pKF->mnId > mnMaxKFid)// keyframe id
+        mnMaxKFid=pKF->mnId;// The id of the largest keyframe
 }
 
 void Map::AddMapPoint(MapPoint *pMP)
 {
     unique_lock<mutex> lock(mMutexMap);
-    mspMapPoints.insert(pMP);// set 集合 插入 地图点
+    mspMapPoints.insert(pMP);// set collection, insert map points
 }
 
 void Map::EraseMapPoint(MapPoint *pMP)
 {
     unique_lock<mutex> lock(mMutexMap);
     
-    // wyw 添加
-  //      auto  it = mspMapPoints.find(pMP);// 获取指针
-  //      delete *it;//删除
+  //      auto  it = mspMapPoints.find(pMP);// get pointer
+  //      delete *it;//delete
     
-    mspMapPoints.erase(pMP);// 删除了指针  内容清除？
+    mspMapPoints.erase(pMP);// pointer is deleted
 
     // TODO: This only erase the pointer.
     // Delete the MapPoint
@@ -48,11 +47,10 @@ void Map::EraseKeyFrame(KeyFrame *pKF)
 {
     unique_lock<mutex> lock(mMutexMap);
     
-        // wyw 添加
-   //     auto  it = mspKeyFrames.find(pKF);// 获取指针
-   //     delete *it;//删除
-        
-    mspKeyFrames.erase(pKF);// 删除了指针  内容清除？
+
+   //     auto  it = mspKeyFrames.find(pKF);// get pointer
+   //     delete *it;//delete
+    mspKeyFrames.erase(pKF);// pointer is deleted
 
     // TODO: This only erase the pointer.
     // Delete the MapPoint
@@ -76,39 +74,39 @@ int Map::GetLastBigChangeIdx()
     return mnBigChangeIdx;
 }
 
-// 返回所有关键帧
+// return all keyframes
 vector<KeyFrame*> Map::GetAllKeyFrames()
 {
     unique_lock<mutex> lock(mMutexMap);
     return vector<KeyFrame*>(mspKeyFrames.begin(),mspKeyFrames.end());
 }
 
-// 返回所有地图点
+// return to all map points
 vector<MapPoint*> Map::GetAllMapPoints()
 {
     unique_lock<mutex> lock(mMutexMap);
     return vector<MapPoint*>(mspMapPoints.begin(),mspMapPoints.end());
 }
 
-// 地图中地图点的数量
+// The number of map points in the map
 long unsigned int Map::MapPointsInMap()
 {
     unique_lock<mutex> lock(mMutexMap);
     return mspMapPoints.size();
 }
-// 地图中 关键帧数量
+// In the map, the number of keyframes
 long unsigned int Map::KeyFramesInMap()
 {
     unique_lock<mutex> lock(mMutexMap);
     return mspKeyFrames.size();
 }
-// 地图中 参考地图点
+// Reference map points in the map
 vector<MapPoint*> Map::GetReferenceMapPoints()
 {
     unique_lock<mutex> lock(mMutexMap);
     return mvpReferenceMapPoints;
 }
-// 最大 id的 关键帧 的 id
+// The id of the keyframe with the largest id
 long unsigned int Map::GetMaxKFid()
 {
     unique_lock<mutex> lock(mMutexMap);
@@ -118,12 +116,12 @@ long unsigned int Map::GetMaxKFid()
 void Map::clear()
 {
     for(set<MapPoint*>::iterator sit=mspMapPoints.begin(), send=mspMapPoints.end(); sit!=send; sit++)
-        delete *sit;// 删除地图点内容
+        delete *sit;// Delete map point content
 
     for(set<KeyFrame*>::iterator sit=mspKeyFrames.begin(), send=mspKeyFrames.end(); sit!=send; sit++)
-        delete *sit;// 删除 关键帧
+        delete *sit;// delete keyframe
 
-    mspMapPoints.clear();//清楚全部
+    mspMapPoints.clear();//clear all
     mspKeyFrames.clear();
     mnMaxKFid = 0;
     mvpReferenceMapPoints.clear();
